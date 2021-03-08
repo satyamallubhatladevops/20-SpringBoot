fd8174e5625e80c0d0c74af2ce5d0cb8ce886a2f
=SatyaPrakash <= satyamallubhatladevops@gmail.com>
2021-03-08 09:33:06 +0530

    MyLocal Files are moving to Centralized Repository

---
+++ src/Sample1.java
@@ -0,0 +1,11 @@
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("This is my First Java Application");
+	}
+
+}
